784f277a9b878573d04fe46124450224eff8af47
Tarcisio Dev <tarcisioliboni@pop-os.localdomain>
2022-07-31 15:32:38 -0300

    process.env

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -7,13 +7,13 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
 );
 
 const strangerThingsConfig = {
-  url: 'https://tarcisioliboni-up.herokuapp.com',
-  timeout: 3000,
+  url: process.env.REACT_APP_HAWKINS_URL,
+  timeout: process.env.REACT_APP_HAWKINS_TIMEOUT,
 };
 
 const upsideDownConfig = {
-  url: 'https://tarcisioliboni-dw.herokuapp.com',
-  timeout: 3000,
+  url: process.env.REACT_APP_UPSIDEDOWN_URL,
+  timeout: process.env.REACT_APP_UPSIDEDOWN_TIMEOUT,
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
